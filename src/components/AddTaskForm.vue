a25b9a74aaad59e82803e134ef35d9fc51da9081
Oleksandr Livitchuk <olivi@softserveinc.com>
2022-09-04 23:07:48 +0300

    feat: added logic for AddTaskForm.vue

--- src/components/AddTaskForm.vue
+++ src/components/AddTaskForm.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="add-task" :class="{ focused: inputIsFocused }">
     <div class="add-task__inner">
-      <button type="button" class="add-task__add-btn">
+      <button type="button" class="add-task__add-btn" @click="addNewTask">
         <plusSVG />
       </button>
 
       <label class="add-task__input" for="add-task">
         <input
-          :placeholder="INPUT_PLACEHOLDER"
           type="text"
           id="add-task"
+          v-model="inputValue"
+          :placeholder="INPUT_PLACEHOLDER"
           @focus="inputIsFocused = true"
           @blur="inputIsFocused = false"
+          @keydown.enter="addNewTask"
         />
       </label>
     </div>
@@ -21,7 +23,15 @@
 <script setup lang="ts">
 import plusSVG from "@/assets/icons/plus.svg";
 import { INPUT_PLACEHOLDER } from "@/components/constants";
-import { ref } from "vue";
+import { ref, defineEmits } from "vue";
+
+const emits = defineEmits(["emitAddNewTask"]);
 
 const inputIsFocused = ref<boolean>(false);
+const inputValue = ref<string>("");
+
+const addNewTask = () => {
+  emits("emitAddNewTask", inputValue.value.trim());
+  inputValue.value = "";
+};
 </script>
